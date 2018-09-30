d512c0076796dadb1add6923ced73134b474fe56
dalinhuang99 <dhuan023@gmail.com>
2018-09-30 17:07:48 -0400

    linter cleanup

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-"use strict";
+'use strict';
 
 // DB etc...
-const config        = require('./config/conf.json')
+const config = require('./config/conf.json');
 
-const express       = require('express');
-const session       = require('express-session');
-const cookieParser  = require('cookie-parser');
-const cookie        = require('cookie');
-const bodyParser    = require('body-parser');
-const morgan        = require('morgan');
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-const app           = express();
-const port          = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Auth
-const passport      = require('passport');
+const passport = require('passport');
 
 // display msg in session
-const flash         = require('connect-flash');
+const flash = require('connect-flash');
 
 
 /* ------------------------------------------- */
-if(process.env.NODE_ENV == undefined) {
+if (process.env.NODE_ENV == undefined) {
   process.env.NODE_ENV = 'development';
   console.log(`
 ================================================
@@ -33,79 +32,80 @@ if(process.env.NODE_ENV == undefined) {
 |   NODE_ENV=development node app.js
 |
 ================================================
-`   );
+` );
 }
 
 // log request to console
 app.use(morgan('dev'));
 // public folder
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // to support JSON-encoded bodies (for auth)
 app.use(cookieParser());
 
- // For Passport
+// For Passport
 app.use(session({
   secret: config[process.env.NODE_ENV].session_secret,
   resave: true,
-  saveUninitialized:true
+  saveUninitialized: true,
 })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
-app.use(flash()) // flash messages stored in session
+app.use(flash()); // flash messages stored in session
 
 
 /**
  *  view
  */
 app.set('view engine', 'ejs');
-app.set('views', './app/views')
+app.set('views', './app/views');
 
 
 /**
  *  models
  */
-const models = require("./app/models");
+const models = require('./app/models');
 
 
 /**
  *  routes
  */
- //load passport strategies
-const passportRoute = require('./app/config/passport/passport.js')(passport,models.User);
+// load passport strategies
+require('./app/config/passport/passport.js')(passport, models.User);
 
-const authRoute = require('./app/routes/auth_routes.js')(app,passport);
+require('./app/routes/auth_routes.js')(app, passport);
 
-const apiV1 = require('./app/routes/v1_0/messages_routes.js')(app);
+// load messages API v1.0
+require('./app/routes/v1_0/messages_routes.js')(app);
 
 
 /**
  *  database
  */
-models.sequelize.sync().then(function(){
-  console.log('Database Connected!')
-}).catch(function(err){
-  console.log(err, "Something went wrong with the Database Update!")
+models.sequelize.sync().then(function() {
+  console.log('Database Connected!');
+}).catch(function(err) {
+  console.log(err, 'Something went wrong with the Database Update!');
 });
 
 
 /**
  *  home page
  */
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   // if user is auth in the session, carry on
   if (req.isAuthenticated()) {
     // load the index.ejs
     res.render('index.ejs', {
-      user: req.user
-    })
+      user: req.user,
+    });
   } else {
     res.render('index.ejs', {
-      user: false
-    })
+      user: false,
+    });
   }
 });
 
@@ -113,12 +113,12 @@ app.get('/', function (req, res) {
 // Route not found (404)
 app.use(function(req, res, next) {
   return res.status(404).send({
-    "meta": {
-      "result": false,
-      "total": 0,
-      "code": 404,
-      "msg": `Oh no, we lost! Route ${req.url} Not found.`
-    }
+    'meta': {
+      'result': false,
+      'total': 0,
+      'code': 404,
+      'msg': `Oh no, we lost! Route ${req.url} Not found.`,
+    },
   });
 });
 
@@ -126,20 +126,20 @@ app.use(function(req, res, next) {
 // other error
 app.use(function(err, req, res, next) {
   return res.status(500).send({
-    "meta": {
-      "result": false,
-      "total": 0,
-      "code": 500,
-      "msg": err
-    }
+    'meta': {
+      'result': false,
+      'total': 0,
+      'code': 500,
+      'msg': err,
+    },
   });
 });
 
 
 /* Start app --------------------------------- */
-var server = app.listen(port, '127.0.0.1', function () {
-  let host = server.address().address
-  let port = server.address().port
+const server = app.listen(port, '127.0.0.1', function() {
+  let host = server.address().address;
+  let port = server.address().port;
 
   console.log(`${process.env.NODE_ENV} running on http://${host}:${port}`);
 });
