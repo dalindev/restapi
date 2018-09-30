35b725d016aa85362cbb87c8894eb4b7824a1e5b
dalinhuang99 <dhuan023@gmail.com>
2018-09-29 22:34:06 -0400

    added get one message endpoint, isPalindrome precalculation and on demand

--- app/controllers/v1_0/messages_controller.js
+++ app/controllers/v1_0/messages_controller.js
@@ -1,7 +1,10 @@
+"use strict";
+
 var exports = module.exports = {}
+var emojiStrip = require('emoji-strip');
 const models = require("../../models");
 
-exports.errResponse = function (res, result, errCode, msg='', data='') {
+exports.errResponse = function(res, result, errCode, msg=null, data=null) {
   return res.status(errCode).send({
     "meta": {
       "result": result,
@@ -12,19 +15,44 @@ exports.errResponse = function (res, result, errCode, msg='', data='') {
   })
 };
 
-exports.getMessages = function(req,res){
+exports.isPalindrome = function(s='ab') {
+  let tmp = emojiStrip(s).toLowerCase().replace(/[^a-zA-Z0-9]+/g, "").split("");
+  let lo = 0;
+  let hi = tmp.length-1;
+
+  while (lo < hi) {
+      if (tmp[lo] === tmp[hi]) {
+          lo++;
+          hi--;
+      } else {
+          return false;
+      }
+  }
+
+  return true;
+}
+
+exports.getMessages = function(req,res) {
 
   const Messages = models.Messages;
 
   res.setHeader('Content-Type', 'application/json');
 
-  // console.log(req.params);
+  const getOneMsg = req.params && req.params.id ? req.params.id : null;
 
   const whereClause = {status: 1};
+  if (getOneMsg) {
+    if (!Number.isInteger(+req.params.id) || +req.params.id < 1 ) {
+      return exports.errResponse(res, false, 400,
+        'Error: Message id should be interger and greater than 0'
+      );
+    }
+    whereClause.id = getOneMsg
+  }
 
-  // TODO - pagination
+  // TODO - rate limit
+  // TODO - pagination limit/page (ex: 20/0 )
   // TODO - ondemand, ex: load msg after id 300
-  // TODO - sort
   Messages.findAll({
     order: [
       ['id', 'ASC']
@@ -35,7 +63,8 @@ exports.getMessages = function(req,res){
       'content',
       'createdAt',
       'User.first_name',
-      'User.last_name'
+      'User.last_name',
+      'palindrome'
     ],
     where: whereClause,
     include: [{
@@ -43,6 +72,16 @@ exports.getMessages = function(req,res){
       attributes: ['first_name','last_name']
     }]
   }).then( msgs => {
+    // edge case - palindrome == null
+    if (getOneMsg &&
+      msgs[0] &&
+      msgs[0].dataValues &&
+      msgs[0].dataValues.palindrome == null
+    ) {
+      // TODO - should update DB for this row since we calculated isPalindrome
+      msgs[0].dataValues.palindrome = exports.isPalindrome(msgs[0].dataValues.content);
+    }
+
     return exports.errResponse(res, true, 200, 'ok', msgs);
   }).catch( err => {
     console.log(err);
@@ -50,18 +89,23 @@ exports.getMessages = function(req,res){
   });
 }
 
-exports.postMessage = function(req,res){
+exports.postMessage = function(req,res) {
+
   const Messages = models.Messages;
 
-  // 400 Bad Request
-  // 413 Payload Too Large
-  // TODO - content size limit
+  // TODO - 413 Payload Too Large
+  // - content size limit
 
-  if(req.body && req.body.data && req.body.data.message) {
+  if (req.body && req.body.data && req.body.data.message) {
+
+    // precalculation
+    let isPalin = exports.isPalindrome(req.body.data.message) || false;
+
     Messages.create({
       user_id: req.user.id,
       content: req.body.data.message,
-    }).then(msg => {
+      palindrome: isPalin
+    }).then( msg => {
       return exports.errResponse(res, true, 201, 'Created', msg);
     }).catch( err => {
       console.log(err);
